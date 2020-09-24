9d91e651daf99cd8b858346a7aa72bf2f978c220
Juan Diego Alejandro Valencia Peña <juandiegoalejandro@hotmail.com>
2020-09-23 19:14:31 -0500

    [new]Changing stack order using setTimeout

---
+++ 0x0C-Javascript_advanced/11-prime.js
@@ -0,0 +1,21 @@
+function countPrimeNumbers(){
+    for (var counter=2; counter<=100; counter++) {
+        var prime = true;
+        for (var div=2; div<=(counter-1); div++) {
+            if (counter % div === 0) {
+                prime = false;
+            }
+        }
+        if (prime === true) {
+            return(counter);
+        }
+    }
+}
+var t0 = performance.now();
+setTimeout(function() {
+    for (i = 0; i < 100; i++){
+        countPrimeNumbers();
+    }  
+},0)
+var t1 = performance.now();
+console.log("Execution time of printing countPrimeNumbers was "+ (t1 - t0) + " milliseconds.");
