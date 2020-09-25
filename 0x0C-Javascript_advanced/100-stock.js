5e8a10b8ab0fae00c83f785ad7b807a9d8a2f840
Juan Diego Alejandro Valencia Peña <juandiegoalejandro@hotmail.com>
2020-09-24 19:29:02 -0500

    [new] Multiple callbacks

---
+++ 0x0C-Javascript_advanced/100-stock.js
@@ -0,0 +1,31 @@
+var stock = {
+    macbook: 2,
+    iphone: 4,
+  };
+  
+  function processPayment(itemName) {
+      stock[itemName] -= 1;
+      console.log(`Payment is being processed for item ${itemName}`);
+  }
+  
+  function processError(itemName) {
+    console.log(`No more ${itemName} in stock`);
+    console.log('Payment is not being processed');
+  }
+  
+  function processOrder(itemName, callbackPayment, callbackError) {
+      console.log(`Verifying the stock of ${itemName}`);
+      product = itemName.toLowerCase();
+  
+      if (stock[product] > 0) {
+        callbackPayment(product);
+      } else {
+      callbackError(itemName);
+      }
+  }
+  
+  
+  document.addEventListener("DOMContentLoaded", function(event) {
+      let name = prompt('Please enter the item you would like to purchase (Macbook, iPhone)');
+      processOrder(name, processPayment, processError);
+  })
